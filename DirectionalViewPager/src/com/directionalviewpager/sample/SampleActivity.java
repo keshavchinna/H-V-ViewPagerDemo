82faca5d4eb26d6b838e5d4a85fea2be26a346ca
keshavchinna <y.keshav@rocketmail.com>
2014-08-01 23:59:56 +0530

    implemented page indicator

--- DirectionalViewPager/src/com/directionalviewpager/sample/SampleActivity.java
+++ DirectionalViewPager/src/com/directionalviewpager/sample/SampleActivity.java
@@ -5,73 +5,58 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.widget.Toast;
 import com.directionalviewpager.DirectionalViewPager;
+import com.viewpagerindicator.CirclePageIndicator;
 
 public class SampleActivity extends FragmentActivity {
 
-  DirectionalViewPager pager;
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
-
-    //Set up the pager
-    pager = (DirectionalViewPager) findViewById(R.id.pager);
-    //pager.setOrientation(DirectionalViewPager.VERTICAL);
-    pager.setOnTouchListener(new MyOnSwipeTouchListener(this));
-    pager.setAdapter(new TestFragmentAdapter(getSupportFragmentManager()));
-
-    //Bind to control buttons
-   /* ((Button) findViewById(R.id.horizontal)).setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        pager.setOrientation(DirectionalViewPager.HORIZONTAL);
-      }
-    });
-    ((Button) findViewById(R.id.vertical)).setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        pager.setOrientation(DirectionalViewPager.VERTICAL);
-      }
-    });
-*/
-
-
-  }
-
-
-  private void showToast(String message) {
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-  }
-
-  private class MyOnSwipeTouchListener extends OnSwipeTouchListener {
-    public MyOnSwipeTouchListener(Context c) {
-      super(c);
-    }
+    DirectionalViewPager pager;
+    CirclePageIndicator mIndicator;
 
     @Override
-    public void onSwipeDown() {
-      pager.setOrientation(DirectionalViewPager.VERTICAL);
-      showToast("Swiped down!");
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.simple_circles);
+        pager = (DirectionalViewPager) findViewById(R.id.pager);
+        //pager.setOrientation(DirectionalViewPager.VERTICAL);
+        pager.setOnTouchListener(new MyOnSwipeTouchListener(this));
+        pager.setAdapter(new TestFragmentAdapter(getSupportFragmentManager()));
+        mIndicator = (CirclePageIndicator) findViewById(R.id.indicator);
+        mIndicator.setViewPager(pager);
     }
 
-    @Override
-    public void onSwipeLeft() {
-      pager.setOrientation(DirectionalViewPager.HORIZONTAL);
-      showToast("Swiped left!");
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    public void onSwipeRight() {
-      pager.setOrientation(DirectionalViewPager.HORIZONTAL);
-      showToast("Swiped right!");
-    }
+    private class MyOnSwipeTouchListener extends OnSwipeTouchListener {
+        public MyOnSwipeTouchListener(Context c) {
+            super(c);
+        }
 
-    @Override
-    public void onSwipeUp() {
-      pager.setOrientation(DirectionalViewPager.VERTICAL);
-      showToast("Swiped up!");
+        @Override
+        public void onSwipeDown() {
+            pager.setOrientation(DirectionalViewPager.VERTICAL);
+            showToast("Swiped down!");
+        }
+
+        @Override
+        public void onSwipeLeft() {
+            pager.setOrientation(DirectionalViewPager.HORIZONTAL);
+            showToast("Swiped left!");
+        }
+
+        @Override
+        public void onSwipeRight() {
+            pager.setOrientation(DirectionalViewPager.HORIZONTAL);
+            showToast("Swiped right!");
+        }
+
+        @Override
+        public void onSwipeUp() {
+            pager.setOrientation(DirectionalViewPager.VERTICAL);
+            showToast("Swiped up!");
+        }
     }
-  }
 
 }
